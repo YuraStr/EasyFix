bd32d4071fc173cbd7f94b9370c88214be077e0e
Yury Strukov <strukov.yura.1996@gmail.com>
2018-05-18 17:15:18 +0300

    replace dropdown with select

--- app/assets/javascripts/promotion_forms.js
+++ app/assets/javascripts/promotion_forms.js
@@ -3,30 +3,27 @@ function addRow() {
         '<td><input type="text" class="form-control" /></td>' +
         '</tr>');
 
-    let dropdown_div = document.createElement("div");
-    dropdown_div.className = "dropdown";
+    let arr = [
+        {val : 1, text: 'One'},
+        {val : 2, text: 'Two'},
+        {val : 3, text: 'Three'}
+    ];
 
-    let dropdown_button = document.createElement("a");
-    dropdown_button.className = "btn btn-default dropdown-toggle";
-    dropdown_button.id = "typesDropdownMenu";
-    dropdown_button.role = "button";
-    dropdown_button.setAttribute("aria-haspopup", "true");
-    dropdown_button.setAttribute("aria-expanded", "false");
-    dropdown_button.appendChild(document.createTextNode("undefined"));
+    let select_div = $('<div>');
 
-    dropdown_div.append(dropdown_button);
+    let sel = $('<select>').addClass('form-control');
+    $(arr).each(function() {
+        sel.append($("<option>").attr('value',this.val).text(this.text));
+    });
 
-    // let dropdown = $('<div class="dropdown"><a class="btn btn-default dropdown-toggle" id="dropdownMenuButton" role="button" aria-haspopup="true" aria-expanded="false">----</a>' +
-    //     '<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">' +
-    //     '<a class="dropdown-item">First item</a>' +
-    //     '</div>' +
-    //     '</div>');
     let trash = $('<td><a class="link-remove-row"><span class=\'glyphicon glyphicon-trash\'></span></a></td>');
 
     trash.click(function (event) {
         $(event.currentTarget).closest("tr").remove();
     });
-    row.append(dropdown_div).append(trash);
+
+    select_div.append(sel);
+    row.append($('<td>').append(select_div)).append(trash);
 
     $("#members_table tr:last").after(row);
 }
